589321f98b162f348eed4415aef0945229c8cb47
giselegarcia <80002777+giselegarcia@users.noreply.github.com>
2021-10-10 09:01:04 -0300

    Update aula06_tiposprimitivos.py
    Nova sintaxe do pycharm version 3

--- exercicios/aula06_tiposprimitivos.py
+++ exercicios/aula06_tiposprimitivos.py
@@ -1,5 +1,5 @@
 n1 = float( input('Digite o primeiro número: '))
 n2 = float( input('Digite o segundo número: '))
 s= n1+n2
-print(' A soma dos números é: ', (s) )
+print(' A soma dos números é: {} '.format (s) ))
 

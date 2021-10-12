c376eaea783b4da3c09acbaa54ff6d3418e4dc41
giselegarcia <80002777+giselegarcia@users.noreply.github.com>
2021-10-12 14:45:58 -0300

    Create desafio003_somandodoisnumeros.py

---
+++ exercicios/desafio003_somandodoisnumeros.py
@@ -0,0 +1,6 @@
+n1 = (float(input(' Quantos reais você tem no momento? ')))
+n2 = (float(input(' Quantos reais voce ganhou hoje? ')))
+s= (n1+n2)
+print(' Voce tem no momento o total de:  {} '.format(s))
+
+

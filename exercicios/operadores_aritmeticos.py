22e62494be35717a1b0666844560acf4dd9e127c
giselegarcia <80002777+giselegarcia@users.noreply.github.com>
2021-10-13 07:06:39 -0300

    Create operadores_aritmeticos.py

---
+++ exercicios/operadores_aritmeticos.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+s = n1+n2
+m = n1*n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+print('A soma é {}, o produto é {} , a divisão é {:.3f} , a divisão inteira é {}, e a potência é {}'.format (s,m,d,di,e))
